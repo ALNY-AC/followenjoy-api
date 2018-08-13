e1db9521cdabfbec7b33f760a7dbd0f60a46c6ed
Jie <392449439@qq.com>
2018-08-13 18:59:17 +0800

    统计

---
+++ app/Home/Controller/UserNumController.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User2
+ * Date: 2018/8/13
+ * Time: 18:16
+ */
+namespace Home\Controller;
+use Think\Controller;
+
+class UserNumController extends Controller{
+    public function getUserNum(){
+        $time = strtotime('2018-8-13 20:30:00');
+        $user_where['add_time'] = ['gt',$time];
+        $data['users'] = D('user')->where($user_where)->count()+0;
+
+        $order_where['add_time'] = ['gt',$time];
+        $data['orders'] = D('order')->where($order_where)->count()+0;
+
+        $price_where['add_time'] = ['gt',$time];
+        $data['price'] = D('order')->where($order_where)->sum('price')+0;
+        echo json_encode($data);
+        exit();
+    }
+}
