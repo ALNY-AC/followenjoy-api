01df3b47aa56eb91ea60161e68bfa0a95d5a878a
艾尔尼亚-A <1173197065@qq.com>
2018-08-16 09:27:02 +0800

    更新

--- app/Admin/Controller/OrderController.class.php
+++ app/Admin/Controller/OrderController.class.php
@@ -164,15 +164,18 @@ class OrderController extends CommonController{
         // 创建模型
         $Supplier=D('Supplier');// 供货商
         $Order=D('Order');// 订单
+        $User=D('User');// 订单
         $Pay=D('Pay');// 订单
         $Snapshot=D('Snapshot');//快照
         $OrderCoupon=D('OrderCoupon');//优惠券订单关联表
         $OrderAddress=D('OrderAddress');//收货地址库
+        $Logistics=D('Logistics');//物流信息
         
         // ===================================================================================
         // 先取订单
         $orderList=$Order
         ->field('*')
+        ->order('state asc,edit_time desc')
         ->where($where)
         ->limit(($page-1)*$page_size,$page_size)
         ->select();
@@ -191,6 +194,7 @@ class OrderController extends CommonController{
             $addressIds[]=$v['address_id'];//组成地址id
             $payIds[]=$v['pay_id'];//组成支付id
             $supplierIds[]=$v['supplier_id'];//组成供货商id
+            $userIds[]=$v['user_id'];//组成供货商id
         }
         if(count($orderIds)>0){
             
@@ -232,9 +236,29 @@ class OrderController extends CommonController{
             ->select();
             
             // ===================================================================================
+            // 取出买家信息
+            $where=[];
+            $where['user_id']=['in',getIds($userIds)];
+            $userList=$User
+            ->where($where)
+            ->field('*')
+            ->select();
+            
+            // ===================================================================================
+            // 取出物流信息
+            // logisticsList
+            $where=[];
+            $where['order_id']=['in',getIds($orderIds)];
+            $logisticsList=$Logistics
+            ->where($where)
+            ->field('*')
+            ->select();
+            
+            
+            // ===================================================================================
             // 将快照等信息拼接回到订单中
             foreach ($orderList as $k => $v) {
-                $v['add_time']=date('Y/m/d H:i:s',$v['add_time']);
+                $v['add_time']=date('Y-m-d H:i:s',$v['add_time']);
                 // ===================================================================================
                 // 拼回快照
                 foreach ($snapshotList as $x => $z) {
@@ -260,6 +284,7 @@ class OrderController extends CommonController{
                 // 拼回支付信息
                 foreach ($payList as $x => $z) {
                     if($z['pay_id']==$v['pay_id']){
+                        $z['pay_time']=date('Y-m-d H:i:s',$z['pay_time']);
                         $v['payInfo']=$z;
                     }
                 }
@@ -268,6 +293,23 @@ class OrderController extends CommonController{
                 foreach ($supplierList as $x => $z) {
                     if($z['supplier_id']==$v['supplier_id']){
                         $v['supplierInfo']=$z;
+                    }
+                }
+                
+                
+                // ===================================================================================
+                // 拼回买家信息
+                foreach ($userList as $x => $z) {
+                    if($z['user_id']==$v['user_id']){
+                        $v['userInfo']=$z;
+                    }
+                }
+                // ===================================================================================
+                // 拼回物流信息
+                
+                foreach ($logisticsList as $x => $z) {
+                    if($z['order_id']==$v['order_id']){
+                        $v['logisticsInfo']=$z;
                     }
                 }
                 
@@ -305,5 +347,163 @@ class OrderController extends CommonController{
         }
         return $info;
     }
+    
+    
+    // ===================================================================================
+    // 获取单个信息
+    public function info(){
+        
+        // ===================================================================================
+        // 组建基本参数
+        $Order=D('Order');
+        $data=I();
+        $order_id=I('order_id');
+        if(!$order_id){
+            die;
+        }
+        
+        $where=[];
+        $where['order_id']=$order_id;
+        // ===================================================================================
+        // 创建模型
+        $Supplier=D('Supplier');// 供货商
+        $Order=D('Order');// 订单
+        $User=D('User');// 订单
+        $Pay=D('Pay');// 订单
+        $Snapshot=D('Snapshot');//快照
+        $OrderCoupon=D('OrderCoupon');//优惠券订单关联表
+        $OrderAddress=D('OrderAddress');//收货地址库
+        $Logistics=D('Logistics');//物流信息
+        
+        // ===================================================================================
+        // 先取订单
+        $orderInfo=$Order
+        ->field('*')
+        ->where($where)
+        ->find();
+        // ===================================================================================
+        // 统计总数
+        
+        $res['total']=$Order
+        ->where($where)
+        ->count()+0;
+        
+        // ===================================================================================
+        // 组成订单的id
+        $address_id=$orderInfo['address_id'];//组成地址id
+        $pay_id=$orderInfo['pay_id'];//组成支付id
+        $supplier_id=$orderInfo['supplier_id'];//组成供货商id
+        $user_id=$orderInfo['user_id'];//买家id
+        
+        
+        // ===================================================================================
+        // 取快照
+        $where=[];
+        $where['order_id']=['in',getIds($orderIds)];
+        $snapshotInfo=$Snapshot
+        ->where($where)
+        ->field('*')
+        ->select();
+        
+        $snapshotInfo['skuInfo']=$this->getSkuInfo($snapshotInfo);
+        unset($snapshotInfo['s1']);
+        unset($snapshotInfo['s2']);
+        unset($snapshotInfo['s3']);
+        
+        
+        // ===================================================================================
+        // 取收货地址
+        $where=[];
+        $where['address_id']=['in',getIds($address_id)];
+        $addressInfo=$OrderAddress
+        ->where($where)
+        ->field('*')
+        ->find();
+        
+        // ===================================================================================
+        // 取支付信息
+        $where=[];
+        $where['pay_id']=['in',getIds($pay_id)];
+        $payInfo=$Pay
+        ->where($where)
+        ->field('*')
+        ->find();
+        
+        // ===================================================================================
+        // 取出供货商信息
+        // supplier_id
+        $where=[];
+        $where['supplier_id']=['in',getIds($supplier_id)];
+        $supplierInfo=$Supplier
+        ->where($where)
+        ->field('*')
+        ->find();
+        
+        // ===================================================================================
+        // 取出买家信息
+        $where=[];
+        $where['user_id']=['in',getIds($user_id)];
+        $userInfo=$User
+        ->where($where)
+        ->field('*')
+        ->find();
+        
+        // ===================================================================================
+        // 取出物流信息
+        // logisticsList
+        $where=[];
+        $where['order_id']=['in',getIds($order_id)];
+        $logisticsInfo=$Logistics
+        ->where($where)
+        ->field('*')
+        ->find();
+        
+        $orderInfo['addressInfo']=$addressInfo;
+        $orderInfo['payInfo']=$payInfo;
+        $orderInfo['supplierInfo']=$supplierInfo;
+        $orderInfo['userInfo']=$userInfo;
+        $orderInfo['logisticsInfo']=$logisticsInfo;
+        
+        $orderInfo['add_time']=date('Y-m-d H:i:s',[$orderInfo])[0];
+        
+        // ===================================================================================
+        // 输出数据
+        if($orderInfo!==false){
+            $res['res']=1;
+            $res['msg']=$orderInfo;
+        }else{
+            $res['res']=-1;
+            $res['msg']=$orderInfo;
+        }
+        echo json_encode($res);
+    }
+    
+    
+    public function saveInfo(){
+        
+        $order_id=I('order_id');
+        $data=I('data');
+        $where=[];
+        $where['order_id']=$order_id;
+        
+        unset($data['add_time']);
+        
+        $data['edit_time']=time();
+        $Order=D('Order');
+        $result=$Order->where($where)->save($data);
+        
+        if($result!==false){
+            $res['res']=1;
+            $res['msg']=$result;
+        }else{
+            $res['res']=-1;
+            $res['msg']=$result;
+        }
+        echo json_encode($res);
+        
+    }
+    
+    
+    
     
 }
