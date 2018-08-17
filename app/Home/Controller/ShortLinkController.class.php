5b7d81b48c663b06916f65163ecaa4f943a22865
Jie <392449439@qq.com>
2018-08-17 08:41:09 +0800

    倒叙

--- app/Home/Controller/ShortLinkController.class.php
+++ app/Home/Controller/ShortLinkController.class.php
@@ -9,6 +9,7 @@ namespace Home\Controller;
 use Think\Controller;
 
 class ShortLinkController extends Controller{
+    const STAR_URL = '/followenjoy/';
 //    /Home/ShortLink/shortLink
 //长链生成短链
     public function shortLink(){
@@ -32,7 +33,7 @@ class ShortLinkController extends Controller{
         $add['edit_time'] = time();
         $add['key'] = md5($long_url);
         $add['long_url'] = $long_url;
-        $add['sort_url'] = '/SXJ/'.$sort_url;
+        $add['sort_url'] = self::STAR_URL.$sort_url;
         $row = D('short_link')->add($add);
         if(!$row){
             $res['res'] = -1;
@@ -46,9 +47,20 @@ class ShortLinkController extends Controller{
 //    /Home/ShortLink/getUrl
 //跳转原始地址
     public function getUrl(){
+        $a = '/aaaa/';
+
+        $count = strpos($a,"/");  //strpos(string,find,start)返回所筛选的字符串出现的位置 参数start可选
+
+        $a = substr_replace($a,"",$count,2);//substr_replace(string,replacement,start,length)参数length可选
+
+echo $a;//输出acdfigcd
+die;
         $long_url = I('SXJ','',false);
-        $where['sort_url'] = '/SXJ/'.$long_url;
+        $where['sort_url'] = self::STAR_URL.$long_url;
         $res = D('short_link')->where($where)->getField('long_url');
+        if(!$res){
+
+        }
         header("Location: $res");
     }
 }
