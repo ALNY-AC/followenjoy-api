90e992d73fcc882543a84f02b5573cbfa644f581
Jie <392449439@qq.com>
2018-08-21 17:52:21 +0800

    aa

--- app/Home/Controller/ShortLinkController.class.php
+++ app/Home/Controller/ShortLinkController.class.php
@@ -9,8 +9,8 @@ namespace Home\Controller;
 use Think\Controller;
 
 class ShortLinkController extends Controller{
-    const STAR_URL = '/followenjoy/';
-//    /Home/ShortLink/shortLink
+    const STAR_URL = '/followenjoy/';//接受参数的
+
 //长链生成短链
     public function shortLink(){
         $long_url = I('long_url','',false);
@@ -47,19 +47,17 @@ class ShortLinkController extends Controller{
 //    /Home/ShortLink/getUrl
 //跳转原始地址
     public function getUrl(){
-        $a = '/aaaa/';
+        $data = self::STAR_URL;
 
-        $count = strpos($a,"/");  //strpos(string,find,start)返回所筛选的字符串出现的位置 参数start可选
+        $data = substr($data,1);
+        $data = substr($data, 0, -1);
 
-        $a = substr_replace($a,"",$count,2);//substr_replace(string,replacement,start,length)参数length可选
-
-echo $a;//输出acdfigcd
-die;
-        $long_url = I('SXJ','',false);
+        $long_url = I($data,'',false);
         $where['sort_url'] = self::STAR_URL.$long_url;
         $res = D('short_link')->where($where)->getField('long_url');
-        if(!$res){
-
+        if(!$res){//找不到分享链接
+            echo "<h1>页面找不到啦<h1>";
+            die;
         }
         header("Location: $res");
     }
